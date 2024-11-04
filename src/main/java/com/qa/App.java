9994f540a6dc911307613add95d0464f3f6e1722
john <johnedmondosn83@yahoo.co.uk>
2024-11-04 14:32:22 +0000

    added

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("JOHN"));
+        System.out.println(sayHelloToSomeone("JOHN12345"));
         System.out.println(sayGoodbye());
     }
 
